ea471b33d4a0cd7f7c248e4fe894fd2a831eb3ff
emmyobonyo@gmail.com <emmyobonyo@gmail.com>
2021-11-19 13:50:33 +0300

    Add MyList class

---
+++ my_list.rb
@@ -0,0 +1,22 @@
+require './MyEnumerable'
+
+class MyList
+  include MyEnumerable
+
+  def initialize(*args)
+    @list = args
+  end
+
+  def each(&block)
+    @list.each(&block)
+  end
+end
+
+list = MyList.new(1, 2, 3, 4)
+p list
+p list.all? {|e| e < 5}
+p list.all? {|e| e > 5}
+p list.any? {|e| e == 2}
+p list.any? {|e| e == 5}
+p list.filter? {|e| e.even?}
+
